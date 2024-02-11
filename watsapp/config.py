dd248a60616b6482f92735f97ee965cc36ac1bd6
Khaled Akel <124167245+KhaledAkel@users.noreply.github.com>
2024-02-11 22:52:13 +0100

    Create config.py

---
+++ watsapp/config.py
@@ -0,0 +1,8 @@
+# Vonage accounts
+endpoint_vonage_message_sandbox = "https://messages-sandbox.nexmo.com/v0.1/messages"
+endpoint_vonage_message_send = "https://messages-sandbox.nexmo.com/v1/messages"
+vonage_sandbox_number = "14157386102"  # Replace with your actual Vonage sandbox number
+vonage_authorization_header = "Basic NmZjOWQ4MmQ6WkE1VnRWSEJYU2VmcDNhWg=="  # The base64-encoded credentials
+
+# OpenAI accounts
+openai_key = "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"  # Your OpenAI API Key
