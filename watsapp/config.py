718279836fd62122273f1253271ba83a67d8b941
Riyad Mazari <126206749+riyadmazari@users.noreply.github.com>
2024-02-18 17:43:13 +0100

    Update config.py
    remove my key

--- watsapp/config.py
+++ watsapp/config.py
@@ -1,8 +1,8 @@
 # Vonage accounts
 endpoint_vonage_message_sandbox = "https://messages-sandbox.nexmo.com/v0.1/messages"
 endpoint_vonage_message_send = "https://messages-sandbox.nexmo.com/v1/messages"
-vonage_sandbox_number = "14157386102"  # Replace with your actual Vonage sandbox number
-vonage_authorization_header = "Basic ZGYyNDUxZTQ6dWFDWDNzVkpoZUZpS0ljaw=="  # The base64-encoded credentials
+vonage_sandbox_number = ""  # Replace with your actual Vonage sandbox number
+vonage_authorization_header = ""  # The base64-encoded credentials
 
 # OpenAI accounts
-openai_key = "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"  # Your OpenAI API Key
+openai_key = ""  # Your OpenAI API Key
