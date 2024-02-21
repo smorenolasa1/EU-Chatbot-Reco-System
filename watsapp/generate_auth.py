9aafb4a9cd364482d0bbda71bc7296c9e24b7cd8
smorenolasa1 <117740777+smorenolasa1@users.noreply.github.com>
2024-02-21 16:50:36 +0100

    Update generate_auth.py

--- watsapp/generate_auth.py
+++ watsapp/generate_auth.py
@@ -1,8 +1,8 @@
 import base64
 
 
-api_key = '0bba93a2'  # Your API key from Vonage
-api_secret = '1dECQaSlgmqJ0s4H'  # Your API secret from Vonage
+api_key = ''  # Your API key from Vonage
+api_secret = ''  # Your API secret from Vonage
 
 
 encoded_credentials = base64.b64encode(f"{api_key}:{api_secret}".encode('utf-8')).decode('utf-8')
