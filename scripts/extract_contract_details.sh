39db78e525500993872ec28066bba6a2c5113e6b
beaamoo <beatrice.mossberg@gmail.com>
2024-02-21 15:27:53 +0100

    check config directory in script

--- scripts/extract_contract_details.sh
+++ scripts/extract_contract_details.sh
@@ -3,8 +3,14 @@
 # Make sure jq is installed in your CI/CD environment
 
 CONTRACT_JSON=./build/contracts/ReportStorage.json
-OUTPUT_CONFIG=./config/contractDetails.json
+OUTPUT_DIR=./config
+OUTPUT_CONFIG=$OUTPUT_DIR/contractDetails.json
 NETWORK_ID=80001  # Adjust based on the target network
+
+# Check if the output directory exists, create it if not
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+fi
 
 CONTRACT_ADDRESS=$(jq -r ".networks[\"$NETWORK_ID\"].address" $CONTRACT_JSON)
 CONTRACT_ABI=$(jq '.abi' $CONTRACT_JSON)
